0d24b5d5e15e7c29095d01dfb081cec0b3dd3a21
kaden <kadenco0de@gmail.com>
2024-01-18 21:59:20 +0900

    init commit

---
+++ cloneTODOLIST/Task.swift
@@ -0,0 +1,15 @@
+//
+//  task.swift
+//  cloneTODOLIST
+//
+//  Created by 박나연 on 1/17/24.
+//
+
+import Foundation
+
+struct Task{
+    var title : String  //할 일 내용
+    var done : Bool     //할 일이 완료되었는지 여부
+}
+
+
